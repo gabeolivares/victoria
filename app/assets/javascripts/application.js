e024719095f3c9e9fc1a65b76d759610b2f82013
Gabe Olivares <gabeolivares@gabes-mbp.attlocal.net>
2017-01-21 15:41:21 -0600

    First commit with content.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
+//= require jquery.min
+//= require skel.min
+//= require util
+//= require main
 //= require_tree .
